c0a2c850f6c47fa682e4c705de8076695be0a58b
Danilo Chang <ray2501@gmail.com>
2018-06-01 05:31:58 +0800

    Add System.Data.Odbc test

---
+++ testapp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Odbc;
+
+namespace testapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "DSN=PostgreSQL; UID=postgres; PWD=postgres;";
+            string sql = "SELECT VERSION()";
+
+            using (var conn= new OdbcConnection(connectionString)) {
+                conn.Open();
+                OdbcCommand comm = new OdbcCommand(sql, conn);
+                OdbcDataReader dr = comm.ExecuteReader();
+                while (dr.Read()) {
+                    Console.WriteLine(dr.GetValue(0).ToString());
+                }
+            }
+        }
+    }
+}
