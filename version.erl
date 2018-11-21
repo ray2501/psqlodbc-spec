27a27a4c0a4c388fc7362acd8756443500ef2bf9
Danilo Chang <ray2501@gmail.com>
2018-11-22 00:03:26 +0800

    Update

---
+++ version.erl
@@ -0,0 +1,10 @@
+#!/usr/bin/env escript
+-export([main/1]).
+
+main(_) ->
+   odbc:start(),
+   {ok, Ref} = odbc:connect("DSN=PostgreSQL; UID=postgres; PWD=postgres;", []),
+   {selected, Columnnames, Rows} = odbc:sql_query(Ref, "SELECT VERSION()"),
+   io:format("~s:~n", [lists:nth(1, Columnnames)]),
+   lists:foreach(fun(X) -> io:format("~s~n", [tuple_to_list(X)]) end, Rows),
+   odbc:disconnect(Ref).
